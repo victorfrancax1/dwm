7b0ce0f85969e87709c129525f2f77c5270ad2e5
Victor Ferreira <vfrncx@pm.me>
2023-11-04 14:13:26 -0400

    changing font to iosevka

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int gappx     = 5;        /* gaps size between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=11";
+static const char *fonts[]          = { "iosevka:Bold:size=12" };
+static const char dmenufont[]       = "iosevka:size=11";
 // bg color
 static const char col_gray1[]       = "#222222";
 // inactive window border collor
